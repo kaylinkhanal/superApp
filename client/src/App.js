593eb8c648ca4242db463436f7562efaaeb8fe6a
ryan5481 <ryan5481@me.com>
2023-01-31 23:45:06 +0800

    added registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import ConditionalRoute from "./routes/ConditionalRoute"
-import "./App.css"
+import ConditionalRoute from "./routes/ConditionalRoute";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-    <ConditionalRoute/>
+      <ConditionalRoute />
     </div>
   );
 }
--- client/src/containers/auth/register.js
+++ client/src/containers/auth/register.js
@@ -1,13 +1,83 @@
-const Register =()=> {
-    // create a form here which should have following fields
-    // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number 
-    // 2. password, confirm password, address 
-    // 3. 
+import React from "react";
+import { Formik, Form, useField, ErrorMessage } from "formik";
+import { object, string, ref } from "yup";
+
+const RegisterValidation = object().shape({
+  name: string().required("Required"),
+  email: string()
+    .required("Valid email required")
+    .email("Valid email required"),
+  password: string().min(8, "Required").required("Required"),
+  confirmPassword: string()
+    .required("Please confirm your password")
+    .oneOf([ref("password")], "Passwords do not match"),
+});
+
+const Input = ({ name, label, ...props }) => {
+  const [field, meta] = useField(name);
   return (
-    <div className="App">
-    Login
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold" for={field.name}>
+        {label}
+      </label>
+      <input
+        className={`${
+          meta.error && meta.touched ? "border-red-500" : ""
+        } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+        {...field}
+        {...props}
+      />
+      <ErrorMessage
+        name={field.name}
+        component="div"
+        className="text-red-500 text-xs"
+      />
     </div>
   );
-}
+};
+
+const Register = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center flex-col bg-gray-100">
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={RegisterValidation}
+      >
+        {() => {
+          return (
+            <Form className="bg-white w-6/12 shadow-md rounded px-8 pt-6 pb-8">
+              <Input name="name" label="Name" />
+              <Input name="email" label="Email" />
+              <Input name="password" label="Password" type="password" />
+              <Input
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+              />
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Register
+                </button>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
 
 export default Register;
