6eb46688b984d1f22118b7eb187e56874678a154
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-02-01 07:03:46 +0545

    created riders form

--- client/src/containers/auth/register.js
+++ client/src/containers/auth/register.js
@@ -2,10 +2,11 @@ const Register =()=> {
     // create a form here which should have following fields
     // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number 
     // 2. password, confirm password, address 
-    // 3. 
+    // 3. try to use formik yup
   return (
     <div className="App">
-    Login
+        <input placeholder="email"/><br/>
+        <input placeholder="phone number"/>
     </div>
   );
 }
