329c74240149d534102308b2b02ceec26a8e657e
infy404 <shrijankarki96@gmail.com>
2023-02-01 17:05:38 +0545

    Create Login Form (Issue #6) -Front End validation using Yup Schema
    Minor Changes:
    Format Code using Prettier
    Added MUI Icons

--- client/src/containers/auth/login.js
+++ client/src/containers/auth/login.js
@@ -1,12 +1,74 @@
-import {Link} from "react-router-dom"
+import { TextField, Grid, Button, Container } from "@mui/material";
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
 
-const Login = ()=>{
+const LoginSchema = Yup.object().shape({
+  mobileNumber: Yup.string()
+    .min(10, "Invalid Number")
+    .max(10, "Invalid Number")
+    .required("Required Field!"),
+
+  password: Yup.string().min(8).required("Required Field!"),
+});
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      mobileNumber: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <div className="App">
-    Login form here...
-    <Link to='/register'>Don't have an account yet?</Link>
-    </div>
+    <Container maxWidth="sm" sx={{ marginTop: "10%" }}>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container flexDirection={"column"} spacing={2}>
+          <Grid item>
+            <LoginOutlinedIcon fontSize="large" />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              id="mobileNumber"
+              name="mobileNumber"
+              label="Mobile Number"
+              value={formik.values.mobileNumber}
+              onChange={formik.handleChange}
+              error={formik.touched.mobileNumber && formik.errors.mobileNumber}
+              helperText={formik.touched.mobileNumber && formik.errors.mobileNumber}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              type={"password"}
+              id="password"
+              name="password"
+              label="Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && formik.errors.password}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
+          <Grid item>
+            <Button color="secondary" variant="contained" type="submit">
+              Login
+            </Button>
+          </Grid>
+          <Grid item>
+            <Link to="/register">Don't have an account yet?</Link>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
   );
-}
+};
 
 export default Login;
--- client/src/containers/auth/register.js
+++ client/src/containers/auth/register.js
@@ -1,55 +1,56 @@
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
-import { Button, TextField, Select, MenuItem, InputLabel, Grid } from '@mui/material'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  Grid,
+} from "@mui/material";
 
-const vehicleType = ['Bike', 'Car', 'Van']
+const vehicleType = ["Bike", "Car", "Van"];
 
 const RegisterSchema = Yup.object().shape({
-  fullName: Yup.string()
-    .min(5, 'Too Short')
-    .required('Required'),
-  registerEmail: Yup.string()
-    .email('Invalid Email')
-    .optional(),
+  fullName: Yup.string().min(5, "Too Short").required("Required"),
+  registerEmail: Yup.string().email("Invalid Email").optional(),
   phoneNumber: Yup.string()
-    .min(10, 'Invalid Phone Number')
-    .max(10, 'Invalid Phone Number')
-    .required('Required'),
-  vehicleType: Yup.string()
-    .required('Vehicle Type is Required'),
+    .min(10, "Invalid Phone Number")
+    .max(10, "Invalid Phone Number")
+    .required("Required"),
+  vehicleType: Yup.string().required("Vehicle Type is Required"),
   vehicleNumber: Yup.string()
-    .min(5, 'Invalid Vehicle Number')
-    .required('Required'),
+    .min(5, "Invalid Vehicle Number")
+    .required("Required"),
   password: Yup.string()
-    .required('Please enter your password')
-    .min(8, 'Password must be atleast 8 characters long'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], "Must enter the same password")
-
-})
-
+    .required("Please enter your password")
+    .min(8, "Password must be atleast 8 characters long"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Must enter the same password"
+  ),
+});
 
 const Register = () => {
   // create a form here which should have following fields
-  // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number 
-  // 2. password, confirm password, address 
+  // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number
+  // 2. password, confirm password, address
   // 3.
   const formik = useFormik({
     initialValues: {
-      fullName: '',
-      registerEmail: '',
-      phoneNumber: '',
-      vehicleType: 'Bike',
-      vehicleNumber: '',
-      password: '',
-      confirmPassword: ''
+      fullName: "",
+      registerEmail: "",
+      phoneNumber: "",
+      vehicleType: "Bike",
+      vehicleNumber: "",
+      password: "",
+      confirmPassword: "",
     },
     validationSchema: RegisterSchema,
     onSubmit: (values) => {
-      console.log(values)
-    }
-  })
-
+      console.log(values);
+    },
+  });
 
   return (
     <div>
@@ -57,10 +58,10 @@ const Register = () => {
         <h1> Register Form </h1>
         <Grid
           container
-          alignContent={'center'}
-          justifyContent={'center'}
+          alignContent={"center"}
+          justifyContent={"center"}
           xl
-          flexDirection={'column'}
+          flexDirection={"column"}
           spacing={2}
         >
           <Grid item width={"50%"}>
@@ -84,8 +85,13 @@ const Register = () => {
               label="Email Address"
               value={formik.values.registerEmail}
               onChange={formik.handleChange}
-              error={formik.touched.registerEmail && Boolean(formik.errors.registerEmail)}
-              helperText={formik.touched.registerEmail && formik.errors.registerEmail}
+              error={
+                formik.touched.registerEmail &&
+                Boolean(formik.errors.registerEmail)
+              }
+              helperText={
+                formik.touched.registerEmail && formik.errors.registerEmail
+              }
             />
           </Grid>
           <Grid item lg>
@@ -94,11 +100,15 @@ const Register = () => {
               id="phoneNumber"
               name="phoneNumber"
               label="Phone Number"
-              type={'number'}
+              type={"number"}
               value={formik.values.phoneNumber}
               onChange={formik.handleChange}
-              error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
-              helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+              error={
+                formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)
+              }
+              helperText={
+                formik.touched.phoneNumber && formik.errors.phoneNumber
+              }
             />
           </Grid>
           <Grid item lg>
@@ -110,13 +120,15 @@ const Register = () => {
               name="vehicleType"
               value={formik.values.vehicleType}
               onChange={formik.handleChange}
-              error={formik.touched.vehicleType && Boolean(formik.errors.vehicleType)}
-              helperText={formik.touched.vehicleType && formik.errors.vehicleType}
+              error={
+                formik.touched.vehicleType && Boolean(formik.errors.vehicleType)
+              }
+              helperText={
+                formik.touched.vehicleType && formik.errors.vehicleType
+              }
             >
               {vehicleType.map((item, index) => {
-                return (
-                  <MenuItem value={item}> {item} </MenuItem>
-                )
+                return <MenuItem value={item}> {item} </MenuItem>;
               })}
             </Select>
           </Grid>
@@ -128,8 +140,13 @@ const Register = () => {
               label="Vehicle Number"
               value={formik.values.vehicleNumber}
               onChange={formik.handleChange}
-              error={formik.touched.vehicleNumber && Boolean(formik.errors.vehicleNumber)}
-              helperText={formik.touched.vehicleNumber && formik.errors.vehicleType}
+              error={
+                formik.touched.vehicleNumber &&
+                Boolean(formik.errors.vehicleNumber)
+              }
+              helperText={
+                formik.touched.vehicleNumber && formik.errors.vehicleType
+              }
             />
           </Grid>
           <Grid item lg>
@@ -154,18 +171,24 @@ const Register = () => {
               type={"password"}
               value={formik.values.confirmPassword}
               onChange={formik.handleChange}
-              error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
-              helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+              error={
+                formik.touched.confirmPassword &&
+                Boolean(formik.errors.confirmPassword)
+              }
+              helperText={
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+              }
             />
           </Grid>
           <Grid item lg>
-            <Button color="primary" variant='contained' type="submit"> Submit </Button>
+            <Button color="primary" variant="contained" type="submit">
+              Submit
+            </Button>
           </Grid>
-        </Grid >
+        </Grid>
       </form>
     </div>
-
   );
-}
+};
 
 export default Register;
