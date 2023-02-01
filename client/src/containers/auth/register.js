1aeb4ba364e22338dca7663d21705e60bd94de74
Sundar 14ry <adoring.alis4249@gmail.comgit config --global user.email adoring.alis4249@gmail.com>
2023-02-01 21:16:08 +0545

    changes in register

--- client/src/containers/auth/register.js
+++ client/src/containers/auth/register.js
@@ -1,13 +1,67 @@
-const Register =()=> {
-    // create a form here which should have following fields
-    // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number 
-    // 2. password, confirm password, address 
-    // 3. 
-  return (
-    <div className="App">
-    Login
-    </div>
-  );
-}
+// const Register =()=> {
+//     // create a form here which should have following fields
+//     // 1. email(optional) 2. phone number 3. full name 4.vehicle type 5. vehicle number 
+//     // 2. password, confirm password, address 
+//     // 3. 
+//   return (
+//     <div className="App">
+//     Login
+//     </div>
+//   );
+// }
 
-export default Register;
+// export default Register;
+
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
+
+const Register = () => {
+  return(
+    
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+}
+export default Register
