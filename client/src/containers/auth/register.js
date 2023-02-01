c7440f69a57e0d50d89edeca71a71bd9734767bc
ryan5481 <ryan5481@me.com>
2023-02-02 00:17:42 +0800

    made changes to register

--- client/src/containers/auth/register.js
+++ client/src/containers/auth/register.js
@@ -64,6 +64,8 @@ const Register = () => {
                 label="Confirm Password"
                 type="password"
               />
+              <Input name="vehicleType" label="vehicleType" />
+              <Input name="vehicleNumber" label="vehicleNumber" />
               <div className="flex items-center justify-between">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
