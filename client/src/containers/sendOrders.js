08132eb78ef7e1ec586b5c5af0476da3922fc0bf
ryan5481 <ryan5481@me.com>
2023-02-07 00:48:55 +0800

    added datePicker to date field in dynamic form

--- client/src/containers/sendOrders.js
+++ client/src/containers/sendOrders.js
@@ -3,6 +3,8 @@ import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import DynamicForm from "../components/Forms/dynamicForm";
 
+const today = new Date();
+console.log(today);
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
     .min(2, "Too Short!")
@@ -13,24 +15,29 @@ const SignupSchema = Yup.object().shape({
     .max(50, "Too Long!")
     .required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
+  exptDeliveryDate: Yup.string()
+    .required("Date is a required field")
+    .min(today, "Invalid date"),
 });
 
 const SendOrders = () => {
   const orderDetailsFields = [
-    { label:"Category", value:"category", type: "text"},
-    { label:"Product Name", value:"productName", type: "text"},
-    { label:"Product Size",value:"productSize",  type: "text"},
-    { label:"Product Description",value:"productDescription",  type: "text"},
-    { label:"Expected Delivery Date",value:"exptDeliveryDate",  type: "text"},
+    { label: "Category", value: "category", type: "text" },
+    { label: "Product Name", value: "productName", type: "text" },
+    { label: "Product Size", value: "productSize", type: "text" },
+    { label: "Product Description", value: "productDescription", type: "text" },
+    {
+      label: "Expected Delivery Date",
+      value: "exptDeliveryDate",
+      type: "date",
+    },
   ];
-
 
   const receiverDetailsFields = [
-    { label:"Receiver Name",value:"receiverName",  type: "text"},
-    { label:"Receiver Address",value:"receiverAddress",  type: "text"},
-    { label:"Phone Number",value:"phoneNumber",  type: "text"},
+    { label: "Receiver Name", value: "receiverName", type: "text" },
+    { label: "Receiver Address", value: "receiverAddress", type: "text" },
+    { label: "Phone Number", value: "phoneNumber", type: "text" },
   ];
-
 
   return (
     <div>
