01fa2f37a9f57fbb4981579d16815809c41f3377
Nishant chaudhary <nishantchaudhary842@gmail.com>
2023-02-10 06:35:34 +0545

    map

--- client/src/containers/auth/login.js
+++ client/src/containers/auth/login.js
@@ -1,15 +1,84 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import * as Yup from 'yup'
+import { Formik, useFormik } from "formik"
+import { Button, TextField, Grid } from '@mui/material'
 
-const Login = ()=>{
+const RegisterSchema = Yup.object().shape({
+  phoneNumber: Yup.string()
+    .min(10, 'Invalid Phone Number')
+    .max(10, 'Invalid Phone Number')
+    .required('Required'),
+  password: Yup.string()
+    .required('Please enter your password')
+    .min(8, 'Password must be atleast 8 characters long'),
+})
+
+
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      phoneNumber: '',
+      password: '',
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
   return (
-    <div className="App">
-    Login form here...
-    <Link to='/register'>Don't have an account yet?</Link>
+    <div style={{height:'100vh',display:'grid', placeContent:'center'}}>
+      <form onSubmit={formik.handleSubmit} style={{border:'2px solid black', width:'400px'}}>
+        <h1> Login </h1>
+        <Grid
+          container
+          alignContent={'center'}
+          justifyContent={'center'}
+          display={"grid"}
+          // flexDirection={'column'}
+          spacing={2}
+        >
+
+          <Grid item lg>
+            <TextField
+              fullWidth
+              id="phoneNumber"
+              name="phoneNumber"
+              label="Phone Number"
+              type={'number'}
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+              helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+            />
+          </Grid>
+
+          <Grid item lg>
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type={"password"}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
+          <Grid item lg>
+            <Button color="primary" variant='contained' type="submit"> Login </Button>
+          </Grid>
+        </Grid >
+      </form>
+     
+     Don't have an account yet? <Link to='/register' value='register'>Register</Link>
     </div>
   );
 }
 
 export default Login;
+
 
 
 
--- client/src/containers/gettingStarted.js
+++ client/src/containers/gettingStarted.js
@@ -1,6 +1,12 @@
 import {useNavigate} from "react-router-dom"
 import {setIsFirstTimeUser} from "../redux/reducers/userSlice"
 import {useDispatch} from "react-redux";
+import CustomCard from "../components/cards/customCard";
+import {
+  Card,
+  Grid,
+} from "@mui/material";
+
 const GettingStarted =()=> {
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -10,7 +16,19 @@ const GettingStarted =()=> {
   }
   return (
     <div className="App">
-       <button onClick={()=> assignVisitor()}>getting started </button>
+    <Grid
+        container
+        flexDirection={"row"}
+        alignContent={"center"}
+        justifyContent={"center"}
+        spacing={2}
+        marginTop={"5%"}
+        backgroundColor={'red'}
+        padding={'100px'}
+      >
+      <button onClick={()=> assignVisitor()} style={{padding:'20px' }}>getting started </button>
+      </Grid>
+       
     </div>
   );
 }
