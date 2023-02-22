dfaa1896c35055c079d384b4ce205340cc64ef83
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-02-22 09:02:01 +0545

    updated comments

--- server/index.js
+++ server/index.js
@@ -44,7 +44,10 @@ const Users = mongoose.model('Users', userSchema);
 
 app.post('/register', async (req, res) => {
   try{
+  //we generate hash(encrpyted password) using bcrypt 
+  // check https://github.com/kelektiv/node.bcrypt.js#:~:text=(myPlaintextPassword%2C-,saltRounds,-).then
   bcrypt.hash(req.body.password, saltRounds).then(async function(hash) {
+    //we set passowrd as new hashed password and save it into db using Save or create
    req.body.password = hash
        const data =  await Users.create(req.body)
     if(data){
