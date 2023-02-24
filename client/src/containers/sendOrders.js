dd4e08f10aafc1cc370dd5f0a00e06e26b1e3bf2
dipubhandari <dipendrabhandari123456@gmail.com>
2023-02-23 19:34:19 -0800

    Old

--- client/src/containers/sendOrders.js
+++ client/src/containers/sendOrders.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
-import { GoogleMap, useJsApiLoader, Marker,Autocomplete } from '@react-google-maps/api';
-import {setSenderCoordinates} from "../redux/reducers/locationSlice"
-import {useDispatch, useSelector} from "react-redux"
+import React, { useState } from 'react'
+import { GoogleMap, useJsApiLoader, Marker, Autocomplete } from '@react-google-maps/api';
+import { setSenderCoordinates } from "../redux/reducers/locationSlice"
+import { useDispatch, useSelector } from "react-redux"
 const containerStyle = {
   width: '100%',
   height: '100vh'
@@ -12,16 +12,17 @@ const center = {
   lng: 85.34456349960001
 };
 
-const SendOrders=()=> {
+const SendOrders = () => {
 
   const [isSenderFormActive, setIsSenderFormActive] = useState(true)
   const [senderAddress, setSenderAddress] = useState('')
-  const {senderCoordinates} = useSelector(state=>state.location)
+  const { senderCoordinates } = useSelector(state => state.location)
   const dispatch = useDispatch()
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ['places'] 
+    googleMapsApiKey: 'AIzaSyDB0KupLJFEHh2G8ZjdDd9KQuo9flSzlek',
+
+    libraries: ['places']
   })
   const [currentCoordinates, setCurrentCoordinates] = useState()
 
@@ -32,7 +33,7 @@ const SendOrders=()=> {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lng = position.coords.longitude;
-      const cordinates = {lat, lng}
+      const cordinates = { lat, lng }
       setCurrentCoordinates(cordinates)
     });
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -47,12 +48,13 @@ const SendOrders=()=> {
   }, [])
 
 
-  const assignSenderLocation = (e)=>{
-    const cordinates = {lat:e.latLng.lat() , lng: e.latLng.lng()}
+  const assignSenderLocation = (e) => {
+    console.log(e)
+    const cordinates = { lat: e.latLng.lat(), lng: e.latLng.lng() }
     dispatch(setSenderCoordinates(cordinates))
-      fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${e.latLng.lat()}&lon=${e.latLng.lng()}&apiKey=a1dd45a7dfc54f55a44b69d125722fcb`)
-      .then(res=> res.json())
-      .then(data=> setSenderAddress(data.features[0].properties.formatted))
+    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${e.latLng.lat()}&lon=${e.latLng.lng()}&apiKey=a1dd45a7dfc54f55a44b69d125722fcb`)
+      .then(res => res.json())
+      .then(data => setSenderAddress(data.features[0].properties.formatted))
   }
 
   return isLoaded ? (
@@ -64,26 +66,26 @@ const SendOrders=()=> {
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        <Marker 
+        <Marker
           draggable={true}
-          onDragEnd={(e)=> assignSenderLocation(e)}
+          onDragEnd={(e) => assignSenderLocation(e)}
           icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
           position={center} />
-        { /* Child components, such as markers, info windows, etc. */ }
+        { /* Child components, such as markers, info windows, etc. */}
         <></>
       </GoogleMap>
-            <div style={{ top:50, margin:'5px', borderRadius:'5%', position:"absolute", width:'20%', height: '40%' , backgroundColor:'aqua' }}> 
+      <div style={{ top: 50, margin: '5px', borderRadius: '5%', position: "absolute", width: '20%', height: '40%', backgroundColor: 'aqua' }}>
         Select your pick up address
         current Browser coords: {JSON.stringify(currentCoordinates)}
         current sender coords: {JSON.stringify(senderCoordinates)}
-        {isSenderFormActive  ? 
-        (<>
-        <Autocomplete>
-        <input placeholder= "Sender address" value={senderAddress} onChange={(e)=> setSenderAddress(e.target.value)}/> 
-        </Autocomplete>
-        <button onClick={()=> setIsSenderFormActive(false)}>Proceed to receiver location</button>
-        </>) :
-        (<input placeholder= "Receiver address"/>)
+        {isSenderFormActive ?
+          (<>
+            <Autocomplete>
+              <input placeholder="Sender address" value={senderAddress} onChange={(e) => setSenderAddress(e.target.value)} />
+            </Autocomplete>
+            <button onClick={() => setIsSenderFormActive(false)}>Proceed to receiver location</button>
+          </>) :
+          (<input placeholder="Receiver address" />)
         }
       </div>
     </>
