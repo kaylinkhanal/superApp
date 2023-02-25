cf864d473ab446911a10c12929f3f48902f81b70
Ramey101 <aryav101@gmail.com>
2023-02-26 07:38:29 +0800

    Order post to backend API endpoint

--- server/index.js
+++ server/index.js
@@ -1,77 +1,63 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 // modules vs commonjs
-const app = express()
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const Users =  require("./models/users")
-const Orders =  require("./models/orders")
+const app = express();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Users = require("./models/users");
+const Orders = require("./models/orders");
 
-const checkFieldType = require('./utils/checkFieldType')
-const connectDb = require('./db/connectDb')
+const checkFieldType = require("./utils/checkFieldType");
+const connectDb = require("./db/connectDb");
 
 const saltRounds = 10;
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+const port = 5001;
 
-const port = 5000
+app.get("/", (req, res) => {
+  res.send("Inital setup");
+});
 
+connectDb();
 
-app.get('/', (req, res) => {
-  res.send('Inital setup')
-})
-
-
-
-connectDb()
-
-
-
-
-
-
-
-
-
-
-app.post('/register', async (req, res) => {
-  try{
-  //we generate hash(encrpyted password) using bcrypt 
-  // check https://github.com/kelektiv/node.bcrypt.js#:~:text=(myPlaintextPassword%2C-,saltRounds,-).then
-  bcrypt.hash(req.body.password, saltRounds).then(async function(hash) {
-    //we set passowrd as new hashed password and save it into db using Save or create
-   req.body.password = hash
-       const data =  await Users.create(req.body)
-    if(data){
-      res.json({
-        message: "Registration successful!!"
-      })
-    }else{
-      res.send('Regsitration failed')
-    }
-  });
-  }catch(err){
-    console.log("err"+err)
+app.post("/register", async (req, res) => {
+  try {
+    //we generate hash(encrpyted password) using bcrypt
+    // check https://github.com/kelektiv/node.bcrypt.js#:~:text=(myPlaintextPassword%2C-,saltRounds,-).then
+    bcrypt.hash(req.body.password, saltRounds).then(async function (hash) {
+      //we set passowrd as new hashed password and save it into db using Save or create
+      req.body.password = hash;
+      const data = await Users.create(req.body);
+      if (data) {
+        res.json({
+          message: "Registration successful!!",
+        });
+      } else {
+        res.send("Regsitration failed");
+      }
+    });
+  } catch (err) {
+    console.log("err" + err);
   }
-})  
+});
 
+app.post("/orders", async (req, res) => {
+  try {
+    console.log(req.body);
+    const data = await Orders.create(req.body);
 
-
-
-app.post('/orders', async (req, res) => {
-  try{
-   //create a orders object/document using Orders model and save it to the database here
-
-  }catch(err){
-    console.log("err"+err)
+    //create a orders object/document using Orders model and save it to the database here
+  } catch (err) {
+    console.log("err" + err);
   }
-})  
+});
 
-const generateToken =async(key, value)=> {
-  try{
+const generateToken = async (key, value) => {
+  try {
     /* [key]: value 
     payload with be something like this(example only):
      username: ram
@@ -80,43 +66,40 @@ const generateToken =async(key, value)=> {
 
      based on this payload + secret key we generate a token and return it back
     */
-    const token = await jwt.sign({ [key]: value }, process.env.SECRET_KEY)
-    return token
-  }catch(err){
-    console.log(err)
+    const token = await jwt.sign({ [key]: value }, process.env.SECRET_KEY);
+    return token;
+  } catch (err) {
+    console.log(err);
   }
+};
 
-}
-
-app.post('/login', async (req, res) => {
-  const fieldKey =  checkFieldType(req.body.loginKey)
-  const token = generateToken(fieldKey, req.body.loginKey)
-  //first we need check if the req.body.loginKey's value exist in the db 
-  const data = await Users.findOne({[fieldKey]: req.body.loginKey})
+app.post("/login", async (req, res) => {
+  const fieldKey = checkFieldType(req.body.loginKey);
+  const token = generateToken(fieldKey, req.body.loginKey);
+  //first we need check if the req.body.loginKey's value exist in the db
+  const data = await Users.findOne({ [fieldKey]: req.body.loginKey });
   //if data is there, it means we found a document in db with that particular phoneNumber
-  if(data){
+  if (data) {
     //we now compare the password(bcrypt lib decypts and compares itself) in db with the one we typed in UI
-    bcrypt.compare(req.body.password, data.password, function(err, result) {
-        if(result){
-          res.json({
-            message: "Login Success!!",
-            token
-          })
-        }else{
-          res.status(401).json({
-            message: "Wrong Password"
-          })
-        }
+    bcrypt.compare(req.body.password, data.password, function (err, result) {
+      if (result) {
+        res.json({
+          message: "Login Success!!",
+          token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Wrong Password",
+        });
+      }
     });
-  }else{
+  } else {
     res.status(403).json({
-      message: "Invalid credentials"
-    })
+      message: "Invalid credentials",
+    });
   }
-})
-
-
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- server/models/orders.js
+++ server/models/orders.js
@@ -1,12 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
+  receiverName: { type: String },
   receiverAddress: { type: String, required: true },
   senderAddress: { type: String },
   receiverPhoneNumber: { type: String },
-})
-const Orders = mongoose.model('Orders', orderSchema);
-module.exports = Orders
+  itemName: { type: String },
+  category: { type: String },
+  weight: { type: Number },
+  itemDiscriptions: { type: String },
+  pickupDate: { type: String },
+  pickupTime: { type: String },
+  acceptedTerms: { type: Boolean },
+});
+const Orders = mongoose.model("Orders", orderSchema);
+module.exports = Orders;
 
-
-
+// receiverAddress: 'Pathivara Khana and Khaja Ghar, Munibhairab Marg, Subidhanagar, Kathmandu 44703, Nepal',
+//   senderAddress: 'Sadhavab Pharma, Shri Ganesh Marg, Subidhanagar, Kathmandu 44703, Nepal',
+//   receiverName: 'katie',
+//   receiverPhoneNumber: '1234567890',
+//   itemName: 'jacket',
+//   category: 'Clothing',
+//   weight: 10,
+//   itemDescription: 'This a wonderful jacket for my brother. ',
+//   pickupDate: '2023-02-01',
+//   pickUpTime: 'Afternoon',
+//   acceptedTerms: true
+// }
