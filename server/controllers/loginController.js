e4b2000911d06244bf9fe03665bc3c44c648d010
Ambica <ambicana5@gmail.com>
2023-02-27 14:04:31 +0545

    login based on username, phonenumber, emailId and generate token

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,34 +4,44 @@ const bcrypt = require('bcrypt');
 const checkFieldType = require('../utils/checkFieldType')
 const saltRounds = 10;
 
+const generateToken = async (key, value) => {
+    console.log(key, value)
+    const token = jwt.sign({ [key]: value }, process.env.SECRET_KEY)
+    return token
+}
+
 const loginUser = async (req, res) => {
-    const token = jwt.sign({ name: req.body.userName }, process.env.SECRET_KEY)
-    const loginKey = checkFieldType(req.body.loginKey)
+    try {
+        const fieldKey = checkFieldType(req.body.fieldKey)
+        const token = generateToken(fieldKey, req.body.fieldKey)
 
-    //first we need check if the req.body.loginKey exist in the db
-    const data = await Users.findOne({ [loginKey]: req.body.loginKey })
-    //if data is there, it means we found a document in db with that particular phoneNumber
-    console.log(data)
+        //first we need check if the req.body.loginKey exist in the db
+        const data = await Users.findOne({ [fieldKey]: req.body.fieldKey })
+        //if data is there, it means we found a document in db with that particular phoneNumber
+        // console.log(data)
 
-    if (data) {
-        //we now compare the password(bcrypt lib decypts and compares itself) in db with the one we typed in UI
-        bcrypt.compare(req.body.password, data.password, function (err, result) {
-            if (result) {
-                res.json({
-                    userDetails: data,
-                    message: "Login Success!!",
-                    token: token
-                })
-            } else {
-                res.json({
-                    message: "Invalid Password!"
-                })
-            }
-        });
-    } else {
-        res.json({
-            message: "user doesn't exist"
-        })
+        if (data) {
+            //we now compare the password(bcrypt lib decypts and compares itself) in db with the one we typed in UI
+            bcrypt.compare(req.body.password, data.password, function (err, result) {
+                if (result) {
+                    res.json({
+                        userDetails: data,
+                        message: "Login Success!!",
+                        // token
+                    })
+                } else {
+                    res.json({
+                        message: "Invalid Password!"
+                    })
+                }
+            });
+        } else {
+            res.json({
+                message: "user doesn't exist"
+            })
+        }
+    } catch (err) {
+        console.log(err)
     }
 }
 
