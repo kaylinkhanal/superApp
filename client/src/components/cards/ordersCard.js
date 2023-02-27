8dd8f98571d1e42c0fbe8b536ffc045c1eb1423d
Ambica <ambicana5@gmail.com>
2023-02-27 23:46:41 +0545

    fixed order list UI

--- client/src/components/cards/ordersCard.js
+++ client/src/components/cards/ordersCard.js
@@ -1,14 +1,22 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+import { useState } from 'react';
 
 const OrdersCard = (props) => {
 	return (
-		<div className="card">
-			<div className="card_content">
-				<h5 className="card_title btn"><span>{props.item.itemName}</span></h5>
-				<h5 className="card_title btn"><span>{props.item.weight}</span></h5>
+		<>
+
+			<div className="orders" >
+				<div className="order_content">
+					<p>Category: <span>{props.item.category}</span></p>
+					<p>Item Name: <span>{props.item.itemName}</span></p>
+					<p>Item Weight: <span>{props.item.weight}</span></p>
+					<p>Receiver Name: <span>{props.item.receiverName}</span></p>
+					<p>Receiver Phone: <span>{props.item.receiverPhoneNumber}</span></p>
+					<p>Pickup Date: <span>{props.item.pickupDate}</span></p>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
