59bae989f0fb135b0cf510099a2102389909aa19
ryan5481 <ryan5481@me.com>
2023-03-03 01:33:54 +0800

    Segregated User Model, Added Colors To Alert SnackBar and Centered Alert SnackBar

--- client/src/containers/sharedScreens/orderList.js
+++ client/src/containers/sharedScreens/orderList.js
@@ -1,26 +1,29 @@
-import { useState, useEffect } from "react"
-import { useSelector } from "react-redux"
-import axios from "axios";
-import OrdersCard from "../../components/cards/ordersCard";
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+import OrdersCard from '../../components/cards/ordersCard'
 const OrderList = () => {
-    const { id, userRole } = useSelector(state => state.user)
-    const [orderList, setOrderList] = useState([])
-    const fetchOrders = async () => {
-        const res = await axios.get(`http://localhost:5000/orders/${userRole=='rider'? '' : id }`)
-        setOrderList(res.data?.ordersList)
-    }
-    useEffect(() => {
-        //we fetch list of orders in the initial load
-        fetchOrders()
-    }, [])
-
-    return (
-        <div>
-            {orderList.length> 0 && orderList.map((item, id) => {
-                return <OrdersCard item={item} />
-            })}
-        </div>
+  const { id, userRole } = useSelector(state => state.user)
+  const [orderList, setOrderList] = useState([])
+  const fetchOrders = async () => {
+    const res = await axios.get(
+      `http://localhost:8000/orders/${userRole == 'rider' ? '' : id}`
     )
+    setOrderList(res.data?.ordersList)
+  }
+  useEffect(() => {
+    //we fetch list of orders in the initial load
+    fetchOrders()
+  }, [])
+
+  return (
+    <div>
+      {orderList.length > 0 &&
+        orderList.map((item, id) => {
+          return <OrdersCard item={item} />
+        })}
+    </div>
+  )
 }
 
 export default OrderList
